e4249a298f4384c6112eb098f1b5d7f2ff6c59b8
Hilbert <hilbertmendez@gmail.com>
2026-07-02 05:27:50 -0600

    feat(frontend): fuerza cambio de password en el primer login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import { AuthProvider } from './auth/AuthContext'
 import { ProtectedRoute } from './auth/ProtectedRoute'
+import { RequirePasswordChanged } from './auth/RequirePasswordChanged'
 import { AppLayout } from './components/AppLayout'
 import { LoginPage } from './routes/LoginPage'
+import { ChangePasswordPage } from './routes/ChangePasswordPage'
 import { DashboardPage } from './features/dashboard/DashboardPage'
 import { ResultsPage } from './features/results/ResultsPage'
 import { RacesPage } from './features/races/RacesPage'
@@ -20,13 +22,17 @@ function App() {
           <Route path="/resultados/:token" element={<PublicResultsPage />} />
 
           <Route element={<ProtectedRoute allowedRoles={['Administrador', 'Lector']} />}>
-            <Route element={<AppLayout />}>
-              <Route path="/" element={<DashboardPage />} />
-              <Route path="/carreras" element={<RacesPage />} />
-              <Route path="/carreras/:raceId" element={<RaceDetailPage />} />
-              <Route path="/resultados" element={<ResultsPage />} />
-              <Route path="/notificaciones" element={<NotificationsPage />} />
-              <Route path="/enlaces" element={<PublicLinksPage />} />
+            <Route path="/change-password" element={<ChangePasswordPage />} />
+
+            <Route element={<RequirePasswordChanged />}>
+              <Route element={<AppLayout />}>
+                <Route path="/" element={<DashboardPage />} />
+                <Route path="/carreras" element={<RacesPage />} />
+                <Route path="/carreras/:raceId" element={<RaceDetailPage />} />
+                <Route path="/resultados" element={<ResultsPage />} />
+                <Route path="/notificaciones" element={<NotificationsPage />} />
+                <Route path="/enlaces" element={<PublicLinksPage />} />
+              </Route>
             </Route>
           </Route>
 
